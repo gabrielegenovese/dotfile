643f0fac60975d930e0ad1d849037f00b8c8db3e
geno <gabrigeno@gmail.com>
2022-11-02 17:15:45 +0100

    base

---
+++ nvim/init.lua
@@ -0,0 +1,28 @@
+require 'visimp' {
+  languages = {
+    -- 'ampl',
+    -- 'bash',
+    'c',
+    -- 'csharp',
+    -- 'css',
+    -- 'dart',
+    'go',
+    -- 'haskell',
+    -- 'html',
+    -- 'java',
+    -- 'javascript',
+    'json',
+    -- 'latex',
+    -- 'lua',
+    -- 'php',
+    'python',
+    -- 'toml'
+  },
+
+  theme = {
+    package = 'gruvbox-community/gruvbox',
+    colorscheme = 'gruvbox',
+    background = 'dark',
+    lualine = 'gruvbox',
+  },
+}
